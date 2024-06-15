faadb7b2ed734ee382d39924c1265fd3e12dc9c8
brendanrauch <brendanjrauch@gmail.com>
2024-06-15 12:59:31 -0600

    hw

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program!");
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int square = number * number;
+
+        Console.WriteLine($"{name}, the sqaure of your number is {square}");
+
+
     }
 }
