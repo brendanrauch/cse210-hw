ab5b08a58d6e9c52dba01ba504bc2edba3597b24
brendanrauch <brendanjrauch@gmail.com>
2024-06-19 12:24:48 -0600

    done

---
+++ sandbox/Sandbox/Circle.cs
@@ -0,0 +1,36 @@
+using System.Runtime.InteropServices;
+
+class Circle
+{
+    private double _radius;
+    
+    public Circle(double radius = 0.0)
+    {
+        _radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+
+    public double GetDiameter()
+    {
+        return  _radius * 2;
+    }
+
+    public double GetCircumfrence()
+    {
+        return 2 * _radius * Math.PI;
+    }
+
+    public double GetRadius()
+    {
+        return _radius * 2;
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,15 +1,17 @@
 using System;
+using System.Data;
 
 class Program
 {
-    // static void Main(string[] args)
-    // {
-    //     Console.WriteLine("Hello Sandbox World!");
-    //     Console.WriteLine("My name is Brendan");
-    // }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Prep1 World!");
 
-    // Console.WriteLine("BOnju")
-    // Double area = CalcualteArea()
+        Circle myCircle = new Circle(10);
+        Console.WriteLine($"{myCircle.GetArea()}");
+
+        Circle unitCircle = new Circle(1);
+        Console.WriteLine($"{unitCircle.GetCircumfrence()}");
+        Console.WriteLine($"{unitCircle.GetDiameter()}");
+    }
 }
-
-// hello 
